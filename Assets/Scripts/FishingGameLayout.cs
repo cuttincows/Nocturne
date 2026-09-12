996ae281258d5ed6c25d6a3a8feaa8993b14abf5
Jessica Thomson <cuttincows@gmail.com>
2026-09-11 19:12:25 -0700

    Got dialogue working, enable the Dialogue object in the ship scene to test it

--- Assets/Scripts/FishingGameLayout.cs
+++ Assets/Scripts/FishingGameLayout.cs
@@ -4,34 +4,32 @@ using UnityEngine.UI;
 
 public class FishingGameLayout : DialogueLayout
 {
-    public Image speakerPortrait;
+    public GameObject speakerPortrait;
+    public Image speakerImage;
     public TextMeshProUGUI speakerName;
-    public TextMeshProUGUI speakerNameBG;
 
     protected override void OnEnable()
     {
         base.OnEnable();
-        speakerPortrait.enabled = false;
+        speakerPortrait.SetActive(false);
     }
 
     public override void ShowSpeaker(Speaker speaker)
     {
         if (speaker == null)
         {
-            speakerPortrait.enabled = false;
+            speakerPortrait.SetActive(false);
             speakerName.enabled = false;
             return;
         }
-         speakerPortrait.enabled = true;
+         speakerPortrait.SetActive(true);
         speakerName.enabled = true;
-        speakerNameBG.enabled = true;
     }
 
     public void HideSpeaker(string _)
     {
-        speakerPortrait.enabled = false;
+        speakerPortrait.SetActive(false);
         speakerName.enabled = false;
-        speakerNameBG.enabled = false;
     }
 
     public override void SetSpeakerEmotion(Speaker speaker, string emotion)
@@ -39,19 +37,19 @@ public class FishingGameLayout : DialogueLayout
         if (speaker == null)
         {
             speakerName.enabled = false;
-            speakerPortrait.enabled = false;
-            speakerNameBG.enabled = false;
+            speakerPortrait.SetActive(false);
             return;
         }
 
         speakerName.enabled = true;
-        speakerPortrait.enabled = true;
-        speakerNameBG.enabled = true;
+        speakerName.text = speaker.fullName;
+
+        speakerPortrait.SetActive(true);
         foreach (Sprite curEmotion in speaker.emotions) 
         {
             if (curEmotion.name.ToLower() == emotion.ToLower()) 
             {
-                speakerPortrait.sprite = curEmotion;
+                speakerImage.sprite = curEmotion;
                 break;
             }
         }
