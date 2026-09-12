ca9765901b198408f51b8c6a900fd50498221886
Jessica Thomson <cuttincows@gmail.com>
2026-09-11 19:34:52 -0700

    Added stub fish types and a switch for them in the behavior tree for specifying more complex actions

--- Assets/Behaviors/FishFleesAction.cs
+++ Assets/Behaviors/FishFleesAction.cs
@@ -8,14 +8,29 @@ using Unity.Properties;
 [NodeDescription(name: "Fish flees ", story: "Fish flees", category: "Action", id: "255644b3a06176775ef0d151e72d20a3")]
 public partial class FishFleesAction : Action
 {
+    private BehaviorGraphAgent agent;
+    private Fish fish;
 
     protected override Status OnStart()
     {
+        agent = GameObject.GetComponent<BehaviorGraphAgent>();
+        fish = GameObject.GetComponent<Fish>();
+
         return Status.Running;
     }
 
     protected override Status OnUpdate()
     {
+        switch(fish)
+        {
+            case SalmonFish fish:
+                break;
+            case CircuitFryFish fish:
+                break;
+            case BassFish fish:
+                break;
+        }
+
         return Status.Success;
     }
 
